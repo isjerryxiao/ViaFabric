ad84aee5218446a7f01c99ccf7b8172b92e26f32
Jerry <isjerryxiao@outlook.com>
2022-12-25 17:36:57 +0800

    build for mc 1.19

--- viafabric-mc119/build.gradle.kts
+++ viafabric-mc119/build.gradle.kts
@@ -3,5 +3,5 @@ dependencies {
     mappings("net.fabricmc:yarn:1.19+build.1:v2")
 
     modImplementation("net.fabricmc.fabric-api:fabric-api:0.55.1+1.19")
-    modImplementation("com.terraformersmc:modmenu:4.0.0-beta.4")
+    modImplementation("curse.maven:modmenu-308702:3873988")
 }
